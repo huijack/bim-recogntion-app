1a54841433b1a4f2e39c863cd52e47f8f7ece0cd
huijack <jacklim2626@gmail.com>
2024-09-04 21:02:40 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn">Button</button>
+    </>
+  )
+}
+export default App
